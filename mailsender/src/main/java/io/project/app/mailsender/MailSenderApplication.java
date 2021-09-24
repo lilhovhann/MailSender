8ddd38b44844db32d5eec7727e777cfee01f3660
lilhovhann <lilith.hovhann@gmail.com>
2021-09-24 13:39:46 +0400

    added mail sender methods in main

--- mailsender/src/main/java/io/project/app/mailsender/MailSenderApplication.java
+++ mailsender/src/main/java/io/project/app/mailsender/MailSenderApplication.java
@@ -1,5 +1,9 @@
 package io.project.app.mailsender;
 
+import io.micrometer.core.instrument.util.StringUtils;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Properties;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
@@ -8,7 +12,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -25,9 +33,71 @@ public class MailSenderApplication {
     public static void main(String[] args) {
         final SpringApplication application = new SpringApplication(MailSenderApplication.class);
         application.setBannerMode(Banner.Mode.CONSOLE);
-        application.setWebApplicationType(WebApplicationType.SERVLET);
-        application.run(args);
+        application.setWebApplicationType(WebApplicationType.SERVLET);        
+        Environment env = application.run(args).getEnvironment();
+        logApplicationStartup(env);
 
+    }
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername("testformailsender123@gmail.com");
+        mailSender.setPassword("testformailsender8");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isBlank(contextPath)) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info(
+                "\n----------------------------------------------------------\n\t"
+                + "Application '{}' is running! Access URLs:\n\t"
+                + "Local: \t\t{}://localhost:{}{}\n\t"
+                + "External: \t{}://{}:{}{}\n\t"
+                + "Profile(s): \t{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                protocol,
+                serverPort,
+                contextPath,
+                protocol,
+                hostAddress,
+                serverPort,
+                contextPath,
+                env.getActiveProfiles()
+        );
+
+        String configServerStatus = env.getProperty("configserver.status");
+        if (configServerStatus == null) {
+            configServerStatus = "Not found or not setup for this application";
+        }
+        log.info(
+                "\n----------------------------------------------------------\n\t"
+                + "Config Server: \t{}\n----------------------------------------------------------",
+                configServerStatus
+        );
     }
 
     @Bean
